1025882b09444cd71c54e14aa89d2cfc3ab6c9af
Mishika-123 <136199026+Mishika-123@users.noreply.github.com>
2023-06-11 00:35:37 +0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,52 @@
+namespace SpriteKind {
+    export const npc = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    color.setPalette(
+    color.Arcade
+    )
+    scene.setBackgroundImage(assets.image`myImage0`)
+    mySprite = sprites.create(img`
+        . . . . f f f f . . . . . 
+        . . f f f f f f f f . . . 
+        . f f f f f f c f f f . . 
+        f f f f f f c c f f f c . 
+        f f f c f f f f f f f c . 
+        c c c f f f e e f f c c . 
+        f f f f f e e f f c c f . 
+        f f f b f e e f b f f f . 
+        . f 4 1 f 4 4 f 1 4 f . . 
+        . f e 4 4 4 4 4 4 e f . . 
+        . f f f e e e e f f f . . 
+        f e f b 7 7 7 7 b f e f . 
+        e 4 f 7 7 7 7 7 7 f 4 e . 
+        e e f 6 6 6 6 6 6 f e e . 
+        . . . f f f f f f . . . . 
+        . . . f f . . f f . . . . 
+        `, SpriteKind.Player)
+    controller.moveSprite(mySprite)
+    tiles.setCurrentTilemap(tilemap`level1`)
+    mySprite2 = sprites.create(img`
+        . . . . . f f 4 4 f f . . . . . 
+        . . . . f 5 4 5 5 4 5 f . . . . 
+        . . . f e 4 5 5 5 5 4 e f . . . 
+        . . f b 3 e 4 4 4 4 e 3 b f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f 3 3 e b 3 e e 3 b e 3 3 f . 
+        . f 3 3 f f e e e e f f 3 3 f . 
+        . f b b f b f e e f b f b b f . 
+        . f b b e 1 f 4 4 f 1 e b b f . 
+        f f b b f 4 4 4 4 4 4 f b b f f 
+        f b b f f f e e e e f f f b b f 
+        . f e e f b d d d d b f e e f . 
+        . . e 4 c d d d d d d c 4 e . . 
+        . . e f b d b d b d b b f e . . 
+        . . . f f 1 d 1 d 1 d f f . . . 
+        . . . . . f f b b f f . . . . . 
+        `, SpriteKind.npc)
+    controller.moveSprite(mySprite, 100, 100)
+})
+let mySprite2: Sprite = null
+let mySprite: Sprite = null
+game.showLongText("A new generation needs to be started in your world, a mysterious woman asks you to help", DialogLayout.Full)
+game.showLongText("press (A) to help make the city a better place", DialogLayout.Center)
